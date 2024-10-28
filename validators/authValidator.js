d9f11fedbe3cfd959ca2d3c3e3debd0e88b5a700
Jitanshu Shaw <jitanshu.shaw1@gmail.com>
2024-10-28 12:56:03 +0530

    added strong password validation in authValidator

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -5,12 +5,20 @@ const registerValidation = [
   body("email").isEmail().withMessage("Please provide a valid email"),
   body("password")
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long"),
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    .withMessage("Password must contain at least one special character"),
 ];
 
 const loginValidation = [
-    body('email').isEmail().withMessage('Please provide a valid email'),
-    body('password').not().isEmpty().withMessage('Password is required'),
-  ];
+  body("email").isEmail().withMessage("Please provide a valid email"),
+  body("password").not().isEmpty().withMessage("Password is required"),
+];
 
-module.exports = { registerValidation, loginValidation  };
+module.exports = { registerValidation, loginValidation };
